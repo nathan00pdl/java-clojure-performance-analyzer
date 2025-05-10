03e581ead51128fb8d0c1c1623466a94449a6f2d
nathan00pdl <nathanpaiva00@gmail.com>
2025-05-10 17:16:28 -0300

    first commit

---
+++ src/main/java/com/example/financial_calculator_fp/FinancialCalculatorFpApplication.java
@@ -0,0 +1,13 @@
+package com.example.financial_calculator_fp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialCalculatorFpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialCalculatorFpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/financial_calculator_fp/FinancialCalculatorFpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financial_calculator_fp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialCalculatorFpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
