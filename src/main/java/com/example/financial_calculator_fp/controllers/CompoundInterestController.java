16be1b33817092e096cbbffb6a966b375a5093ca
nathan00pdl <nathanpaiva00@gmail.com>
2025-05-14 18:05:35 -0300

    implementation of the controller for calculating compound interest

---
+++ src/main/java/com/example/financial_calculator_fp/controllers/CompoundInterestController.java
@@ -0,0 +1,38 @@
+package com.example.financial_calculator_fp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.financial_calculator_fp.models.request.CompoundInterestRequestDTO;
+import com.example.financial_calculator_fp.models.response.CompoundInterestResponseDTO;
+import com.example.financial_calculator_fp.services.CompoundInterestService;
+
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/api/compound-interest")
+public class CompoundInterestController {
+    private CompoundInterestService compoundInterestService;
+
+    @Autowired
+    public CompoundInterestController(CompoundInterestService compoundInterestService) {
+        this.compoundInterestService = compoundInterestService;
+    }
+
+    @PostMapping("/calculate")
+    public ResponseEntity<CompoundInterestResponseDTO> calculateCompoundInterest (@Valid @RequestBody CompoundInterestRequestDTO request) {
+        CompoundInterestResponseDTO response = compoundInterestService.calculateCompoundInterest(request);
+
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/api/health")
+    public ResponseEntity<String> health() {
+        return ResponseEntity.ok("Serviço de Juros Compostos está funcionando!");
+    }
+}
