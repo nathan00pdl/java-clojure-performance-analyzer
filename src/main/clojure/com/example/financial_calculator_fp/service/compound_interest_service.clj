0a28c6bcba324bbd4ec89d67530b8172a7cddc34
nathan00pdl <nathanpaiva00@gmail.com>
2025-05-22 19:00:45 -0300

    adding dependencies and plugins for load testing with the gatling tool

--- src/main/clojure/com/example/financial_calculator_fp/service/compound_interest_service.clj
+++ src/main/clojure/com/example/financial_calculator_fp/service/compound_interest_service.clj
@@ -7,11 +7,6 @@ Features:
 -Integration with Java - create-service
 "
 
-"
-current balance - saldo atual 
-interest earned - juros ganho
-"
-
 ;; Defining Namespace
 (ns com.example.financial-calculator-fp.service.compound-interest-service
   (:import
@@ -22,11 +17,11 @@ interest earned - juros ganho
 (defn round-to-3-decimals [value]
   (double (/ (Math/round (* value 1000.0)) 1000.0)))
 
-(defn calculate-interest-for-year              ;Name of function
-  "Calculo de juros compostos para um ano"     ;Represents a documentation of the function (Docstring)
-  [initial-amount annual-rate]                 ;List of parameters / Note: Runtime type inference 
-  (let [rate (/ annual-rate 100)               ;Defining bindings - rate is the name of the local variable (let)
-        result (* initial-amount (+ 1 rate))]  ;Formula: P*(1+r)
+(defn calculate-interest-for-year              ; Name of function
+  "Calculo de juros compostos para um ano"     ; Represents a documentation of the function (Docstring)
+  [initial-amount annual-rate]                 ; List of parameters / Note: Runtime type inference 
+  (let [rate (/ annual-rate 100)               ; Defining bindings - rate is the name of the local variable (let)
+        result (* initial-amount (+ 1 rate))]  ; Formula: P*(1+r)
     (round-to-3-decimals result)))          
 
 (defn calculate-interest-for-multiple-years
@@ -36,35 +31,35 @@ interest earned - juros ganho
          current-balance initial-amount
          yearly-details  []] 
     (if (> year years)
-      yearly-details  ;"Return yearly-details if true"
+      yearly-details  ; "Return yearly-details if true"
       (let [new-balance     (calculate-interest-for-year current-balance annual-rate)
             interest-earned (round-to-3-decimals (- new-balance current-balance))
-            year-detail     {:year                year  ;year-detils represents a key/value map
+            year-detail     {:year                year  ; year-detils represents a key/value map
                              :starting-balance    current-balance
                              :interest-earned     interest-earned
                              :contributions-added 0.0
                              :ending-balance      new-balance}]
         (recur (inc year)
                new-balance
-               (conj yearly-details year-detail))))))  ;Adding the element to the collection in an immutable way / "conj" = "conjoins"
+               (conj yearly-details year-detail))))))  ; Adding the element to the collection in an immutable way / "conj" = "conjoins"
 
 (defn process-request
   "Processamento da requisicao e retorno de uma resposta"
   [^CompoundInterestRequestDTO request]
   (let [
         ;; Extraction of request data
-        initial-amount (.getInitialAmount request) ;Java method call
+        initial-amount (.getInitialAmount request)  ; Java method call
         annual-rate (.getAnnualInterestRate request)
         years (.getYears request)
 
-        yearly-details (calculate-interest-for-multiple-years initial-amount annual-rate years) ;Return an array of maps with details for each year
+        yearly-details (calculate-interest-for-multiple-years initial-amount annual-rate years)  ; Return an array of maps with details for each year
 
-        final-balance (:ending-balance (last yearly-details)) ;Get the last element of the array
+        final-balance (:ending-balance (last yearly-details)) ; Get the last element of the array
         total-interest (round-to-3-decimals (- final-balance initial-amount))
 
         ;; Creating java objects for response (yearly-dtos and calculation-dto)
-        yearly-dtos (map (fn [detail]  ;Will return a new collection with the results 
-                           (new YearlyBalanceDTO ;Creating a new instance of this class
+        yearly-dtos (map (fn [detail]  ; Will return a new collection with the results 
+                           (new YearlyBalanceDTO  ; Creating a new instance of this class
                             (int (:year detail))
                             (:starting-balance detail)
                             (:interest-earned detail)
@@ -77,13 +72,12 @@ interest earned - juros ganho
                          total-interest
                          final-balance)]
   
-  ;; Return response object
-  (new CompoundInterestResponseDTO calculation-dto yearly-dtos)))
+  (new CompoundInterestResponseDTO calculation-dto yearly-dtos)))  ; Return response object
 
 ;; Function that will be called by the Java code in the configuration (external use -> ^:export)
 (defn ^:export create-service 
   "Criacao de uma instancia do servico que implementa a interface java"
   []
-  (reify CompoundInterestService  ;Creation of an anonymous instance (object Java) that implements such interface
-    (calculateCompoundInterest [this request] ;Method name defined in Java interface / "request" is the CompoundInterestRequestDTO parameter 
+  (reify CompoundInterestService  ; Creation of an anonymous instance (object Java) that implements such interface
+    (calculateCompoundInterest [this request]  ; Method name defined in Java interface / "request" is the CompoundInterestRequestDTO parameter 
       (process-request request))))
--- src/main/java/com/example/financial_calculator_fp/config/ClojureNamespaceLoader.java
+++ src/main/java/com/example/financial_calculator_fp/config/ClojureNamespaceLoader.java
@@ -24,6 +24,6 @@ public class ClojureNamespaceLoader {
         IFn require = Clojure.var("clojure.core", "require"); 
         require.invoke(Clojure.read("com.example.financial-calculator-fp.service.compound-interest-service"));
  
-        System.out.println("Namespaces Clojure carregados com sucesso");
+        System.out.println("Namespace Clojure carregados com sucesso");
     }
 }
