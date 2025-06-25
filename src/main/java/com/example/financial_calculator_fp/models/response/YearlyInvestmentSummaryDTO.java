ce91955940d7f7d7dbf255d21da55f87ed02dfd8
nathan00pdl <nathanpaiva00@gmail.com>
2025-06-25 19:02:07 -0300

    renaming DTOs and attributes

---
+++ src/main/java/com/example/financial_calculator_fp/models/response/YearlyInvestmentSummaryDTO.java
@@ -0,0 +1,61 @@
+package com.example.financial_calculator_fp.models.response;
+
+public class YearlyInvestmentSummaryDTO {
+    private Integer year;
+    private Double startBalance;
+    private Double endBalance;
+    private Double interestEarned;
+    private Double additionalContribution;
+
+    public YearlyInvestmentSummaryDTO(){}
+
+    public YearlyInvestmentSummaryDTO(Integer year, Double startBalance, Double endBalance, Double interestEarned, Double additionalContribution){
+        this.year = year;
+        this.startBalance = startBalance;
+        this.interestEarned = interestEarned;
+        this.additionalContribution = additionalContribution;
+        this.endBalance = endBalance;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Double getStartBalance() {
+        return startBalance;
+    }
+
+    public void setStartBalance(Double startBalance) {
+        this.startBalance = startBalance;
+    }
+
+    public Double getInterestEarned() {
+        return interestEarned;
+    }
+
+    public void setInterestEarned(Double interestEarned) {
+        this.interestEarned = interestEarned;
+    }
+
+    public Double additionalCetContribution() {
+        return additionalContribution;
+    }
+
+    public void additionalCetContribution(Double additionalContribution) {
+        this.additionalContribution = additionalContribution;
+    }
+
+    public Double getEndBalance() {
+        return endBalance;
+    }
+
+    public void setEndBalance(Double endBalance) {
+        this.endBalance = endBalance;
+    }
+
+    
+}
