a74a4ce98edb753f312069709a3995c1e042b4a7
nathan00pdl <nathanpaiva00@gmail.com>
2025-09-17 17:49:10 +0000

    refactoring of the service implemented in java

--- src/main/java/com/example/java_clojure_performance_analyzer/services/JavaCompoundInterestService.java
+++ src/main/java/com/example/java_clojure_performance_analyzer/services/JavaCompoundInterestService.java
@@ -14,22 +14,15 @@ import com.example.java_clojure_performance_analyzer.models.response.YearlyInves
 
 @Service("javaImplementation")
 public class JavaCompoundInterestService implements CompoundInterestService {
-    
     private static final int DECIMAL_PRECISION = 3;
+    private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
 
     private double round(double value) {
-        return BigDecimal.valueOf(value)
-                .setScale(DECIMAL_PRECISION, RoundingMode.HALF_UP)
-                .doubleValue();
+        return BigDecimal.valueOf(value).setScale(DECIMAL_PRECISION, ROUNDING_MODE).doubleValue();
     }
 
     private double applyCompoundInterest(double amount, double rate) {
-        return round(amount * (1 + rate / 100.0));
-    }
-    
-    private YearlyInvestmentSummary createYearlyInvestmentSummary(int year, double startBalance, double endBalance) {
-        double interestEarned = round(endBalance - startBalance);
-        return new YearlyInvestmentSummary(year, startBalance, endBalance, interestEarned);
+        return round(amount * (1.0 + rate / 100.0));
     }
 
     @Override
@@ -38,17 +31,22 @@ public class JavaCompoundInterestService implements CompoundInterestService {
         final double rate = request.getAnnualInterestRate();
         final int years = request.getYears();
 
+        final List<YearlyInvestmentSummary> yearlyDetailsList = new ArrayList<>(years);
+        
         double currentAmount = initialAmount;
-        List<YearlyInvestmentSummary> yearlyDetailsList = new ArrayList<>(years);
 
         for (int year = 1; year <= years; year++) {
-            double newAmount = applyCompoundInterest(currentAmount, rate);
-            yearlyDetailsList.add(createYearlyInvestmentSummary(year, currentAmount, newAmount));
-            currentAmount = newAmount;
+            final double startBalance = currentAmount;
+            final double endBalance = applyCompoundInterest(currentAmount, rate);
+            final double interestEarned = round(endBalance - startBalance);
+            
+            yearlyDetailsList.add( new YearlyInvestmentSummary(year, startBalance, endBalance, interestEarned));
+            
+            currentAmount = endBalance;
         }
 
-        double totalInterestEarned = round(currentAmount - request.getInitialAmount());
-        InvestmentSummary summary = new InvestmentSummary(request.getInitialAmount(), currentAmount, totalInterestEarned);
+        final double totalInterestEarned = round(currentAmount - initialAmount);
+        InvestmentSummary summary = new InvestmentSummary(initialAmount, currentAmount, totalInterestEarned);
 
         return new CompoundInterestResponse(summary, yearlyDetailsList);
     }
