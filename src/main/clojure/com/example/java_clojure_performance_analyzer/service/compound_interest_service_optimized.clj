888f93ac5ad02aa0004772d361c13048ff68d274
nathan00pdl <nathanpaiva00@gmail.com>
2025-09-18 13:34:18 +0000

    refactoring names and rounding locations - clojure implementation

--- src/main/clojure/com/example/java_clojure_performance_analyzer/service/compound_interest_service_optimized.clj
+++ src/main/clojure/com/example/java_clojure_performance_analyzer/service/compound_interest_service_optimized.clj
@@ -7,35 +7,35 @@
 
 (set! *warn-on-reflection* true)
 
-(defn round ^double  
+(defn ^double round-value
   [^double value]
   (let [^BigDecimal bd (BigDecimal/valueOf value)]
     (.doubleValue (.setScale bd 3 RoundingMode/HALF_UP))))
 
-(defn calculate-compound-interest-yearly ^double
+(defn ^double apply-compound-interest
   [^double current-amount, ^double annual-rate]
   (let [rate (/ annual-rate 100.0)
         result (* current-amount (+ 1.0 rate))]
-    (round result)))
+    (round-value result)))
 
-(defn calculate-compound-interest-years 
+(defn calculate-compound-interest-years
   [^double initial-amount, ^double annual-rate, ^long years]
   (loop [^long year 1
          ^double current-amount initial-amount
-         yearly-details-list []] 
+         yearly-details-list []]
     (if (> year years)
       yearly-details-list
-      (let [^double new-amount (calculate-compound-interest-yearly current-amount annual-rate)
-            ^double interest-earned (- new-amount current-amount)
+      (let [^double new-amount (apply-compound-interest current-amount annual-rate)
+            ^double interest-earned (round-value (- new-amount current-amount))
             year-detail {:year            year
                          :start-balance   current-amount
                          :end-balance     new-amount
                          :interest-earned interest-earned}]
         (recur (inc year)
                new-amount
-               (conj yearly-details-list year-detail)))))) 
+               (conj yearly-details-list year-detail))))))
 
-(defn process-compound-interest-request ^CompoundInterestResponse
+(defn ^CompoundInterestResponse calculate-compound-interest-request
   [^CompoundInterestRequest request]
   (let [^double initial-amount (.getInitialAmount request)
         ^double rate           (.getAnnualInterestRate request)
@@ -44,8 +44,8 @@
         yearly-details-list    (calculate-compound-interest-years initial-amount rate years)
         ^clojure.lang.IPersistentMap last-detail (last yearly-details-list)
         ^double current-amount   (:end-balance last-detail)
-        ^double total-interest-earned (- current-amount initial-amount)
-        
+        ^double total-interest-earned (round-value (- current-amount initial-amount))
+
         yearly-summary-results (map (fn [^clojure.lang.IPersistentMap detail]
                                       (new YearlyInvestmentSummary
                                            (int (:year detail))
@@ -58,10 +58,10 @@
                              initial-amount
                              current-amount
                              total-interest-earned)]
-    (new CompoundInterestResponse summary-results (vec yearly-summary-results)))) 
+    (new CompoundInterestResponse summary-results (vec yearly-summary-results))))
 
-(defn ^:export create-service 
+(defn ^:export create-service
   []
   (reify CompoundInterestService
     (calculateCompoundInterest [_ request]
-      (process-compound-interest-request request))))
+      (calculate-compound-interest-request request))))
